bc035a89dc8c7553a8ff96422041e47c4cf64a0e
PARSICO <39967013+parsico@users.noreply.github.com>
2024-05-18 22:53:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,13 @@ NC='\033[0m' # No Color
 echo -e "${GREEN} ░▀░ █▀▀█ █▀▀█ █▀▀█ █░█ █░░█"
 echo -e "${GREEN} ▀█▀ █░░█ █▄▄▀ █░░█ ▄▀▄ █▄▄█"
 echo -e "${GREEN} ▀▀▀ █▀▀▀ ▀░▀▀ ▀▀▀▀ ▀░▀ ▄▄▄█${NC}"
-echo -e "${GREEN}Welcome to the Parsico iProxy!${NC} Please Install iProxy Enter: '1' or Fake-TLS Site list Enter:'2' to proceed."
+echo -e "${GREEN}Welcome to the Parsico iProxy!${NC} Please select an option:"
+echo -e "${GREEN}1. Install iProxy"
+echo -e "2. View Fake-TLS Site List"
+echo -e "3. Create Proxy with Legacy Kernel${NC}"
 echo -e "${GREEN}https://github.com/parsico${NC}"
 
-read -p "Enter your choice 1 / 2: " choice
+read -p "Enter your choice (1 / 2 / 3): " choice
 
 if [ "$choice" = "1" ]; then
     apt update && apt upgrade -y
@@ -21,6 +24,9 @@ if [ "$choice" = "1" ]; then
 elif [ "$choice" = "2" ]; then
     echo -e "${GREEN}Fake TLS domains:${NC}"
     wget -qO- https://raw.githubusercontent.com/parsico/iproxy/main/fake-tls-domain.txt
+elif [ "$choice" = "3" ]; then
+    echo -e "${GREEN}Creating Proxy with Legacy Kernel...${NC}"
+    curl -o MTProtoProxyInstall.sh -L https://git.io/fjo34 && bash MTProtoProxyInstall.sh
 else
     echo "Incorrect command. Script aborted."
 fi
